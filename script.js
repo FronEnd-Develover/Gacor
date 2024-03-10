7959bba52b3f9bdc8d56d92bd3269f5871599436
FronEnd-Develover <146324324+FronEnd-Develover@users.noreply.github.com>
2024-03-10 17:29:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("tampilkanFormulir").addEventListener("click", function() {
+      var formulirPendaftaran = document.querySelector(".daftar-akun");
+      formulirPendaftaran.style.display = "block";
+  });
